3962aab4dbffbc077b53eac1ea255cba16f076f1
Viacheslav.Andreev <slava.avjv@gmail.com>
2024-03-27 21:42:37 +0300

    Edit demo-md5.ch

--- demo_for_md5.sh
+++ demo_for_md5.sh
@@ -2,14 +2,14 @@
 read -p "Enter path for control: "  pathcheck
 read -p "enter pat for result file: " pathresult 
 mkdir -p  $pathresult
-#cd $pathresult 
+cd $pathresult 
 	for var in  $pathcheck/*
 	  do	
 		echo  "file name - " $var
 		md5sum $pathcheck/$var >> $pathresult/$pathcheck-md5sum.txt
 	  done
 
-
+echo "Done"
 
 
 
